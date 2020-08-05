7f55affdb9be864672c97d9b9bcdc176e7d1a448
Yu Hao (Tom) Lai <tomlai8882000@hotmail.com>
2020-08-05 21:41:23 +0000

    Automatically backed up by Learn

--- app/models/model.rb
+++ app/models/model.rb
@@ -1,20 +1,20 @@
-class Models
+class Dog
 
-  # attr_accessor :name, :breed, :age
-  # 
-  # @@all = []
-  # 
-  # def initialize(name, breed, age)
-  #   @name = name
-  #   @breed = breed
-  #   @age = age
-  # 
-  #   @@all << self
-  # end
-  # 
-  # def self.all
-  #   @@all
-  # end
+  attr_accessor :name, :breed, :age
+  
+  @@all = []
+  
+  def initialize(name, breed, age)
+    @name = name
+    @breed = breed
+    @age = age
+  
+    @@all << self
+  end
+  
+  def self.all
+    @@all
+  end
 
 
 end
